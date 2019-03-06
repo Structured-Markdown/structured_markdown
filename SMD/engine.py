4e18992a51d2cafa56a2b40a5f098238cf062f6d
Tloru (Isaac C.) <isaacimagine@gmail.com>
2019-03-06 21:10:09 +0700

    reimplemented engine.py, again.

--- SMD/engine.py
+++ SMD/engine.py
@@ -1,161 +1,146 @@
 import mistune
 
+class Line:
+    __slots__ = ("line", "ind_type", "ind", "ind_level")
+
+    def __init__(self, line, ind_type, ind=None):
+        self.line = line
+        self.ind = ind
+        self.ind_type = ind_type
+
+        if self.ind == None:
+            self._cleanse()
+
+    def __repr__(self): return "Line({}, {}, ind={})".format(self.line, self.ind_type, self.ind)
+    def __str__(self): return self.line
+    def __int__(self): return self.ind
+
+    def _indentation(self): return self.line.replace(self.line.lstrip(), "")
+
+    def _cleanse(self):
+        leading = self._indentation()
+        ind = len(leading) / len(self.ind_type)
+
+        if int(ind) != ind and self.line != "\n" and self.line != "":
+            raise IndentationError("Unexpected indentation type ({}).".format(repr(leading)))
+
+        self.line = self.line.strip()
+        self.ind = int(ind)
+
 class StructuredMarkdown:
-    def __init__(self, fin, valid_ind=None, tokens=None, css_mapping=None):
+    def __init__(self, inp):
         """
-        self: StructuredMarkdown object
-        fin: file input (file or file-like object)
-        valid_ind: list of strings of indentation
-        tokens: dict of tokens mapped to behaviour
-        css_mapping: mapping of SMD names to css names
-        returns: StructuredMarkdown object
+        self: StructuredMarkdown Object
+        inp: string to be processed
+        returns: New StructuredMarkdown Object
         """
-        self.dirty_lines = [str(line) for line in fin]
-
-        self.valid_ind = ["  ", "    ", "\t"] if valid_ind is None else valid_ind
-
-        self.tokens = list(":=") if tokens is None else tokens
-
-        self.css_mapping = {
-            "layer": "div",
-            "all": "*",
-        } if css_mapping is None else css_mapping
-
+        # predefined
+        self.valid_ind = ["\t", "  ", "    "]
+        self.tokens = [
+            ":",
+            "=",
+        ]
         self.keywords = [
             "layer",
             "style",
         ]
 
-        self.SMD_datastructure = None
+        self.dirty = inp.split("\n") # each line in inp
+        self.ind_type = self._infer_ind(self.dirty) # infer the indentation from first indent
+        self.lines = [Line(line, self.ind_type) for line in self.dirty] # create a list of Line objects
 
-        self.ind = self.infer_ind(self.dirty_lines, valid_ind=self.valid_ind)
-        self.lines = self.cleanse(self.dirty_lines, self.ind)
-
-    def __call__(self, html=True, css=True):
-        returned = self._layer(self.lines)
-        if not html:
-            returned.pop(0)
-            if not css:
-                return []
-        if not css:
-            returned.pop(1)
-        return returned
+    def _infer_ind(self, dirty):
+        """
+        self: StructuredMarkdown Object
+        dirty: list of "dirty" lines
+        returns: indentation type of the list self._valid_ind or ""
+        """
+        for line in dirty:
+            leading = line.replace(line.lstrip(), "")
+            if leading in self.valid_ind:
+                return leading
+            if leading != "" and leading != "\n" and leading not in self.valid_ind:
+                raise IndentationError("Indentation type unsupported ({}).".format(repr(leading)))
+        return ""
 
     def _tokenize(self, line):
+        """
+        self: StructuredMarkdown Object
+        line: Line object to tokenize
+        returns: tokenized line, list of strs
+        """
+        line = str(line)
+
         for token in self.tokens:
             line = line.replace(token, " {} ".format(token))
-        return " ".join(line.split()).split()
 
-    def _layer(self, lines, name="root"):
-        markdown = []
-        css = []
-        html = []
+        return line.split()
+
+    def html(self, lines=None, name="root"):
+        """
+        self: StructuredMarkdown Object
+        lines: list of Line Objects to parse, if set to None self.lines is used
+        returns: html extracted from self
+        """
+        html = ""
+        markdown = ""
+
+        if lines is None:
+            lines = self.lines
+
         while len(lines) > 0:
             line = lines.pop(0)
-            tokenized = self._tokenize(line[1])
+            tokenized = self._tokenize(line)
 
-            # markdown detection sandwitch
-            if tokenized == []:
-                markdown.append("")
-            elif tokenized[0] in self.keywords and tokenized[-1] == ":":
-                try: scope_name = tokenized[1:-1]
-                except: raise("No name for scope")
+            if tokenized != [] and tokenized[0] in self.keywords and tokenized[-1] == ":":
+                scope_name = tokenized[1:-1]
+
+                if len(scope_name) > 1:
+                    raise Exception("Name of a layer contains spaces ({}).".format(" ".join(scope_name)))
 
                 scope_name = scope_name[0]
-
-                html.append(self._markdown(markdown))
-                markdown = []
 
                 scope = []
                 while len(lines) > 0:
                     line = lines.pop(0)
-                    if line[0] == 0 and self._tokenize(lines[0][1]) != []:
+                    if int(line) <= 0 and str(line) != "":
+                        lines.insert(0, line)
                         break
-                    scope.append((line[0] - 1, line[1]))
+                    scope.append(Line(str(line), ind_type=self.ind_type, ind=line.ind-1))
 
                 if tokenized[0] == "layer":
-                    if len(scope_name) != 1:
-                        raise Exception("Space in layer name")
-                    html_string, css_string = self._layer(lines, name="scope_name")
-                    html.append(html_string)
-                    css.append(css_string)
-
-                elif tokenized[0] == "style":
-                    css.append(self._style(lines, scope_name))
-
+                    html = html + mistune.markdown(markdown)
+                    markdown = ""
+                    html = html + self.html(scope, name=scope_name)
             else:
-                markdown.append(line[1])
+                markdown = markdown + str(line)
 
-        html.append(self._markdown(markdown))
-        css_string = "\n".join(css) + "\n"
-        html_string = "<div class='{}'>\n".format(name) \
-                      + "\n".join("  " + line for line in html) \
-                      + "\n</div>\n"
+            markdown = markdown + "\n"
 
-        return html_string, css_string
+        html = html + mistune.markdown(markdown)
 
-    def _style(self, lines, selector):
+        return "<div class=\"{}\">\n{}\n</div>\n".format(
+            name, "\n".join(self.ind_type + line for line in html.split("\n")[:-1])
+        )
+
+    def css(self, lines=None):
         """
-        lines: list of tuples of indentation, line content
-        selector: unparsed string of css selector
+        self: StructuredMarkdown Object
+        lines: list of Line Objects to parse, if set to None self.lines is used
+        returns: css extracted from self
         """
+        css = ""
 
-        parsed_selector = []
-        for token in self._tokenize(selector):
-            parsed_selector.append(self.css_mapping.get(token, token))
-        selector = "".join(parsed_selector)
+        if lines is None:
+            lines = self.lines
 
-        parsed_lines = []
-
-        for indentation, line in lines:
-            for token in self._tokenize(line):
-                parsed_lines.append("{}: {};".format(*line.split("=")))
-        lines = parsed_lines
-
-        return selector + " \{\n" \
-               + "\n".join("  " + line for line in lines) + "\n" \
-               + "}\n"
-
-    def _markdown(self, lines):
-        # maybe make escape=True?
-        return mistune.markdown("\n".join(line[1] for line in lines) + "\n", escape=False, hard_wrap=True)
-
-    def _indentation(self, line):
-        return line.replace(line.lstrip(), "")
-
-    def infer_ind(self, dirty_lines, valid_ind):
-        for line_number, line in enumerate(dirty_lines, 1):
-            leading = self._indentation(line)
-            if leading in valid_ind:
-                return leading
-            if leading != "" and leading not in valid_ind:
-                raise Exception("'{}' Indentation type unsupported.").format(repr(leading))
         return ""
 
-    def cleanse(self, dirty_lines, ind):
-        ind_levels = []
-        cleansed = []
-
-        for line_number, line in enumerate(dirty_lines, 1):
-            leading = self._indentation(line)
-            ind_level = len(leading) / len(ind)
-
-            if int(ind_level) != ind_level and line != "\n" and line != "":
-                raise IndentationError(
-                    "'{}' Unexpected indentation type.".format(repr(leading))
-                )
-
-            ind_level = int(ind_level)
-            line = line.strip()
-
-            ind_levels.append(ind_level)
-            cleansed.append(line)
-
-        # make named tuple, maybe?
-        return list(zip(ind_levels, cleansed))
-
 if __name__ == "__main__":
-    file = "example.smd"
-    with open("example.smd", "r") as fin:
-        example = StructuredMarkdown(fin)
-    print(example(css=False))
+    example_file = "example.smd"
+
+    with open(example_file, "r") as fin:
+        inp = fin.read()
+
+    example = StructuredMarkdown(inp)
+    print(example.html())
