049b38864e4041017a74b3dedc7bf1e4ff813bad
Tloru (Isaac C.) <isaacimagine@gmail.com>
2019-03-07 15:08:37 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="structured_markdown",
+      version=0.0,
+      description="Add an extra dimension to your markdown documents",
+      url="https://github.com/Structured-Markdown/structured_markdown",
+      author="Structured-Markdown",
+      author_email="isaacimagine@gmail.com",
+      license="MIT",
+      packages=["structured_markdown"],
+      zip_safe=False,
+
