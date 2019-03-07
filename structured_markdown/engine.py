2a49c49ec6891957b1ac1d1e2d7aa87185c11228
Tloru (Isaac C.) <isaacimagine@gmail.com>
2019-03-07 17:57:35 +0700

    added smd.parse helper function

--- structured_markdown/engine.py
+++ structured_markdown/engine.py
@@ -178,3 +178,22 @@ class StructuredMarkdown:
             css = css + "}\n"
 
         return css
+
+def parse(inp, html=None, css=None, name="root"):
+
+    if html is None and css is None:
+        html, css = True, True
+    elif html is None:
+        html = False
+    elif css is None:
+        css = False
+        
+    smd_instance = StructuredMarkdown(inp)
+    returned = []
+
+    if html:
+        returned.append(smd_instance.html(lines=None, name=name))
+    if css:
+        returned.append(smd_instance.css(lines=None, selector=None))
+
+    return returned
