79d7c08d7de59c10bbec5a2f055e49c3ad235535
Tloru (Isaac C.) <isaacimagine@gmail.com>
2019-03-07 16:07:16 +0700

    update gitignore, expand setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fin:
+    long_description = fin.read()
+
+setuptools.setup(
     name="structured_markdown",
     version=0.0,
     description="Add an extra dimension to your markdown documents",
+    long_description=long_description,
     url="https://github.com/Structured-Markdown/structured_markdown",
     author="Structured-Markdown",
     author_email="isaacimagine@gmail.com",
